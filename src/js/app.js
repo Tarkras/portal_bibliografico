b567ba0d8fcc5b463e0842ce53b7bf04fcb5cc60
Dídac Ortega Escalera <didacoe@gmail.com>
2022-04-10 02:04:02 +0200

    Added slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,41 @@
-
+import KeenSlider from 'keen-slider';
+import 'keen-slider/keen-slider.min.css';
 const dots = document.getElementById('dots');
 const readMore = document.getElementById('read-more');
 const readMoreButton = document.getElementById('read-more-button');
 const icon = document.getElementById('rm-icon');
 
+new KeenSlider("#slider-banner", {
+  loop: true,
+},
+[(slider) => {
+  let timeout
+  let mouseOver = false
+  function clearNextTimeout() {
+    clearTimeout(timeout)
+  }
+  function nextTimeout() {
+    clearTimeout(timeout)
+    if (mouseOver) return
+    timeout = setTimeout(() => {
+      slider.next()
+    }, 2000)
+  }
+  slider.on("created", () => {
+    slider.container.addEventListener("mouseover", () => {
+      mouseOver = true
+      clearNextTimeout()
+    })
+    slider.container.addEventListener("mouseout", () => {
+      mouseOver = false
+      nextTimeout()
+    })
+    nextTimeout()
+  })
+  slider.on("dragStarted", clearNextTimeout)
+  slider.on("animationEnded", nextTimeout)
+  slider.on("updated", nextTimeout)
+},])
 
 function leerMasMenos() {
   if (dots.style.display === "none") {
@@ -33,40 +65,9 @@ if(readMoreButton !== null) {
 
 
 
-// import KeenSlider from 'keen-slider';
-// import 'keen-slider/keen-slider.min.css';
 
-// new KeenSlider("#slider", {
-//   loop: true,
-// },
-// [(slider) => {
-//   let timeout
-//   let mouseOver = false
-//   function clearNextTimeout() {
-//     clearTimeout(timeout)
-//   }
-//   function nextTimeout() {
-//     clearTimeout(timeout)
-//     if (mouseOver) return
-//     timeout = setTimeout(() => {
-//       slider.next()
-//     }, 2000)
-//   }
-//   slider.on("created", () => {
-//     slider.container.addEventListener("mouseover", () => {
-//       mouseOver = true
-//       clearNextTimeout()
-//     })
-//     slider.container.addEventListener("mouseout", () => {
-//       mouseOver = false
-//       nextTimeout()
-//     })
-//     nextTimeout()
-//   })
-//   slider.on("dragStarted", clearNextTimeout)
-//   slider.on("animationEnded", nextTimeout)
-//   slider.on("updated", nextTimeout)
-// },])
+
+
 
 
 
